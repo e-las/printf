073da48fe554a91e05cca123ad6b18b48910ac34
Jeroldine <oakleyjeroldine@gmail.com>
2023-10-18 01:19:04 +0000

    Update main.h

--- main.h
+++ main.h
@@ -38,8 +38,9 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i, 
-	va_list arg_list, char buffer[], int flags, int width,int precision, int size);
+int handle_print(const char *fmt, int *i,
+	va_list arg_list, char buffer[], int flags, int width
+	, int precision, int size);
 
 
 int print_char(va_list types, char buffer[],
